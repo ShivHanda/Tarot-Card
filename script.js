10f4d22683b1ad6ce073b484644f313b19cf76c8
Shiv <99603993+ShivHanda@users.noreply.github.com>
2025-12-19 19:26:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,10 +76,10 @@ function handleCardClick(cardElement, cardName) {
     // --- MAGIC CALCULATION ---
     // 1. Hum slot ki position lete hain screen ke center ke hisaab se
     // 2. 35 isliye kyunki card width (70px) ka aadha hai, taaki center match ho
-    const moveX = slotRect.left - (window.innerWidth / 2) + (slotRect.width / 2) + 8 ; 
+    const moveX = slotRect.left - (window.innerWidth / 2) + (slotRect.width / 2); 
     
     // 3. Deck container se slot kitna upar hai
-    const moveY = slotRect.top - deckRect.top + 4;
+    const moveY = slotRect.top - deckRect.top;
 
     cardElement.style.zIndex = 1000 + slotIdx;
     
