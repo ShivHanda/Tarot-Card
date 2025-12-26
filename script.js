f853e7305fcfdd98e77f068a468bdee6ed573327
Shiv <99603993+ShivHanda@users.noreply.github.com>
2025-12-27 01:22:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function renderDeck() {
         setTimeout(() => {
             const total = allCards.length;
             const center = total / 2;
-            const angle = (index - center) * 1.5;
+            const angle = (index - center) * 0.8;
             const xPos = (index - center) * 7;
             cardDiv.style.transform = `translateX(${xPos}px) rotate(${angle}deg)`;
         }, 500); 
